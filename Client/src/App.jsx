d602a857675fe595580bd5eda10bb44d1ba1be0e
Alae Bouhmala <alaebouhmala@outtlok.com>
2025-04-07 15:50:09 +0100

    Basic configs for project

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
+import React from "react";
+import Home from "./pages/Home/home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Login from "./pages/Login/login";
+import SingUp from "./pages/SingUp/singup";
+import "./index.css";
 
-function App() {
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/dashboard" exact element = {<Home />} />
+      <Route path="/login" exact element = {<Login />} />
+      <Route path="/singup" exact element = {<SingUp />} />
+    </Routes>
+  </Router>
+);
+
+const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
-    </div>
-  );
+    <div>{routes}</div>
+  )
 }
 
 export default App;
-
-
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/*.{js,ts,jsx,tsx,vue,php}",
+    "./src/**/*.{js,ts,jsx,tsx,vue,php}",
   ],
   theme: {
     extend: {},
