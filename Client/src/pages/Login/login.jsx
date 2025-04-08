a8709a13686ae194fd63c61256d652ba5640119d
Alae Bouhmala <alaebouhmala@outtlok.com>
2025-04-08 14:16:25 +0100

    Create login page and singup page

--- Client/src/pages/Login/login.jsx
+++ Client/src/pages/Login/login.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../../components/NavBar";
+import { Link } from "react-router-dom";
+import { validateEmail } from "../../utils/helper";
+import PasswordInput from "../../components/Inputs/PasswordInput";
 
 
-const Login = () =>{
-    return(
-        <NavBar />
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+
+
+    const handleLogin = async (e) => {
+
+        e.preventDefault();
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valide email address!");
+            return;
+        }
+
+        if (!password) {
+            setError("Please Enetr your password!");
+            return;
+        }
+
+        setError("");
+
+    };
+
+
+    return (
+        <>
+            <NavBar />
+
+            <div className="flex items-center justify-center mt-28">
+                <div className="w-96 border rounded bg-white px-7 py-10">
+                    <form onSubmit={handleLogin} className="flex flex-col py-4">
+                        <h4 className="text-2xl mb-7 text-center"> Login </h4>
+                        <input
+                            type="text"
+                            placeholder="Email/Username"
+                            className="input-box py-2 px-2 my-2 border rounded"
+                            value={email}
+                            onChange={(e) => { setEmail(e.target.value) }}
+                        />
+                        <PasswordInput
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                        />
+
+                        {error && <p className="text-red-800 text-xs pb-1">{error}</p>}
+
+                        <button type="submit" className="bg-primary text-white py-2 px-2 my-2 border rounded"> Login </button>
+                        <p className="text-sm text-center mt-4"> Not registered yet? {""}
+                            <Link to="/singup" className="text-primary font-semibold" >
+                                Create Account
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
--- Client/src/pages/SingUp/singup.jsx
+++ Client/src/pages/SingUp/singup.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import NavBar from "../../components/NavBar";
+import { Link } from "react-router-dom";
 
 const SingUp = () =>{
     return (
-        <div>SingUp</div>
+        <>
+            <NavBar />
+
+            <div className="flex items-center justify-center mt-28">
+                <div className="w-96 border rounded bg-white px-7 py-10">
+                    <form onSubmit={() => { }} className="flex flex-col py-4">
+                        <h4 className="text-2xl mb-7 text-center"> SingUp </h4>
+                        <input type="text" placeholder="Name" className="input-box py-2 px-2 my-2 border rounded" />
+                        <input type="text" placeholder="Email" className="input-box py-2 px-2 my-2 border rounded" />
+                        <input type="password" placeholder="Password" className="input-box py-2 px-2 my-2 border rounded" />
+                        <button type="submit" className="bg-primary text-white py-2 px-2 my-2 border rounded"> SingUp </button>
+                        <p className="text-sm text-center mt-4"> Have Account? {""}
+                            <Link to="/login" className="text-primary font-semibold" >
+                                Login
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
 
